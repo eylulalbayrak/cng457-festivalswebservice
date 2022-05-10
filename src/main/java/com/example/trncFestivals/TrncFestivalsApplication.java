6666220591f54a76bb84b5be3937278c9be90958
eylulalbayrak <eylul.albayrak@metu.edu.tr>
2022-05-10 20:29:55 +0300

    Initial commit

---
+++ src/main/java/com/example/trncFestivals/TrncFestivalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.trncFestivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrncFestivalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrncFestivalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trncFestivals/TrncFestivalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trncFestivals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrncFestivalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
