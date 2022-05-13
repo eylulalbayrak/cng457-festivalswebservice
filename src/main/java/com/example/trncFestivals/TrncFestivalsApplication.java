35b3bbbf577dd57047bf929765fc3933f023c5c4
eylulalbayrak <49821130+eylulalbayrak@users.noreply.github.com>
2022-05-13 21:37:17 +0300

    Revert "Finalized Entities (#1)"
    This reverts commit 78f9698fed538bffaf15b91d78bb4182ff31e3d4.

--- src/main/java/com/example/trncFestivals/TrncFestivalsApplication.java
+++ src/main/java/com/example/trncFestivals/TrncFestivalsApplication.java
@@ -1,20 +1,8 @@
-/**
- * @author Eylul Begum Albayrak
- * @author Mohammed S. Elguzoli
- *
- * @Date May 12, 2022
- *
- * CNG457 - Term Project Phase II
- */
-
 package com.example.trncFestivals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main Class for trncFestivals
- */
 @SpringBootApplication
 public class TrncFestivalsApplication {
 
