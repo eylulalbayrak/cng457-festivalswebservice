ccdea073d5b4da2b34b3f8eedcf6621643934d07
eylulalbayrak <eylul.albayrak@metu.edu.tr>
2022-06-10 03:52:47 +0300

    Integration Testing for /organizersearch and /popularfestivals

--- src/test/java/com/example/trncFestivals/controller/IndividualControllerTest.java
+++ src/test/java/com/example/trncFestivals/controller/IndividualControllerTest.java
@@ -2,16 +2,17 @@ package com.example.trncFestivals.controller;
 
 import com.example.trncFestivals.TrncFestivalsApplication;
 import com.example.trncFestivals.entity.Festival;
+import com.example.trncFestivals.entity.Individual;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = TrncFestivalsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class FestivalControllerTest {
+public class IndividualControllerTest {
 
     @Autowired
     TestRestTemplate restTemplate;
@@ -19,15 +20,11 @@ public class FestivalControllerTest {
     @LocalServerPort
     int port;
 
-    @Test
-    void getFestivalByName() {
-        assertEquals(this.restTemplate.getForObject("http://localhost:" + port + "/festivalsbyname?festivalName=Orange", Festival[].class).length, 1);
-    }
 
 //    This test is not properly working. Details are explained in project report.
 //    @Test
-//    void getFestivalsWithHighestRuns() {
-//        assertEquals(this.restTemplate.getForObject("http://localhost:" + port + "/popularfestivals", Festival[].class).length, 1);
+//    void getIndividualByNameOrSurnameOrPhoneNo() {
+//        assertEquals(this.restTemplate.getForObject("http://localhost:"  + port + "/organizersearch?key=Eylul", Individual[].class).length, 1);
 //    }
 
 }
