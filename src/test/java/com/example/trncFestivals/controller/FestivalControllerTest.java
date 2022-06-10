d50fbc1f63047f223fd2313134427ba113790b40
eylulalbayrak <eylul.albayrak@metu.edu.tr>
2022-06-10 03:25:12 +0300

    Integration Testing for /festivalsbyname

---
+++ src/test/java/com/example/trncFestivals/controller/FestivalControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.trncFestivals.controller;
+
+import com.example.trncFestivals.TrncFestivalsApplication;
+import com.example.trncFestivals.entity.Festival;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = TrncFestivalsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FestivalControllerTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    int port;
+
+    @Test
+    void getFestivalByName() {
+        assertEquals(this.restTemplate.getForObject("http://localhost:" + port + "/festivalsbyname?festivalName=Orange", Festival[].class).length, 1);
+    }
+
+//    @Test
+//    void getFestivalsWithHighestRuns() {
+//        assertEquals(this.restTemplate.getForObject("http://localhost:" + port + "/popularfestivals", Festival[].class).length, 1);
+//    }
+
+}
